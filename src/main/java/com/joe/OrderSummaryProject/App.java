5df28a682da01e543f19e62f7ecb311b0cd0e6c1
Joseph Kabubo <joseph.kabubo@gmail.com>
2022-10-03 23:48:25 +0300

    Initial Commit

---
+++ src/main/java/com/joe/OrderSummaryProject/App.java
@@ -0,0 +1,13 @@
+package com.joe.OrderSummaryProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
